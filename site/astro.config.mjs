eb6d7dadaf5c2ce0bb59dbcf02ebf65689df4e81
pikesley <sam.pikesley@gmail.com>
2023-04-04 22:20:23 +0100

    indexable orbs

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
+import htmlBeautifier from "astro-html-beautifier";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://css-foolishness.netlify.app",
+  integrations: [
+    htmlBeautifier({
+      indent_size: 2,
+      wrap_line_length: 88,
+      jslint_happy: true,
+      preserve_newlines: false,
+    }),
+  ],
+});
